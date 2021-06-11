80e8bdcf1358774789321aa370a79193c1b2665b
Debashis Biswas <debashis.biswas@asu.edu>
2021-06-11 12:35:42 -0700

    add pitch accent data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import csv
+from typing import Dict
 import requests
-from requests.models import Response
 
 def _bold(str):
     return f"**{str}**"
@@ -7,19 +8,47 @@ def _bold(str):
 def _spoiler(str):
     return f"||{str}||"
 
-def main():
-    selected_word = input("Which word would you like to use? > ")
-    r: Response = requests.get(f'https://jisho.org/api/v1/search/words?keyword={selected_word}')
+def open_tsv() -> Dict:
+    custom_dict = {}
+    with open("./pitch_data/ja_pitch_accents.tsv") as tsvfile:
+        reader = csv.DictReader(tsvfile, delimiter="\t", quotechar='"')
+        for row in reader:
+            custom_dict[row['word']] = {
+                "kana": row['kana'],
+                "accent": row['accent']
+            }
 
+    return custom_dict
+
+def get_reading(word: str) -> str:
+    custom_dict = open_tsv()
+
+    return custom_dict[word]['kana'] if word in custom_dict else ''
+
+def get_accent(word: str) -> int:
+    custom_dict = open_tsv()
+
+    return custom_dict[word]['accent'] if word in custom_dict else 0
+
+def get_definitions(word: str) -> str:
+    r = requests.get("https://jisho.org/api/v1/search/words", { "keyword": word })
     if r.status_code != 200:
         print(f"Request to Jisho failed.")
         print(f"Status code: {r.status_code}")
         print(r.reason)
         return
 
-    reading = r.json()['data'][0]['japanese'][0]['reading']
-    pitch = 1
-    definition = r.json()['data'][0]['senses'][0]['english_definitions']
+    definitions = []
+    for row in r.json()['data'][0]['senses']:
+        definitions += row['english_definitions']
+
+    return definitions
+
+def main():
+    selected_word = input("Which word would you like to use? > ")
+    reading = get_reading(selected_word)
+    pitch = get_accent(selected_word)
+    definitions = get_definitions(selected_word)
 
     wide_space = "　"
     print(_bold("今日の言葉"))
@@ -29,12 +58,8 @@ def main():
         wide_space +
         _spoiler(f'{reading}　[{pitch}]') +
         wide_space +
-        _spoiler(f'{definition}')
+        _spoiler(f'{"; ".join(definitions)}')
     )
-    print()
-    print(f"では、{_bold('本日')}の練習はこれまでにします。")
-    print(_spoiler(f"では、{_bold('ほんじつ')}の　れんしゅうは　これまでに　します。"))
-    print(_spoiler(f"That will be all for {_bold('today')}'s practice."))
 
 if __name__ == "__main__":
     main()
