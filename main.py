888447d23d8f551552dcdc59323ebcc279c16cc2
Debashis Biswas <debashis.biswas@asu.edu>
2021-06-29 14:16:42 -0700

    added data class for abstraction

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import csv
 from lxml import etree
 
+class Data:
+    def __init__(self, *, jisho_path, tsuneyo_path):
+        self.jisho = self._init_jisho(jisho_path)
+        self.tsuneyo = self._init_tsuneyo(tsuneyo_path)
+
+    def _init_jisho(self, path: str):
+        parser = etree.XMLParser(dtd_validation=True)
+        return etree.parse(path, parser)
+
+    def _init_tsuneyo(self, path: str):
+        custom_dict = {}
+        with open(path) as tsv:
+            reader = csv.DictReader(tsv, delimiter="\t", quotechar='"')
+            for row in reader:
+                custom_dict[row['word']] = {
+                    "kana": row['kana'],
+                    "accent": row['accent']
+                }
+        return custom_dict
+
+    def get_jisho(self):
+        return self.jisho
+
+    def get_tsuneyo(self):
+        return self.tsuneyo
+
 def _bold(str):
     return f"**{str}**"
 
@@ -19,30 +45,34 @@ def open_tsv():
 
     return custom_dict
 
-def get_reading(word: str) -> str:
-    custom_dict = open_tsv()
+def get_reading(word: str, *, data: Data) -> str:
+    custom_dict = data.get_tsuneyo()
 
     return custom_dict[word]['kana'] if word in custom_dict else ''
 
-def get_accent(word: str) -> int:
-    custom_dict = open_tsv()
+def get_accent(word: str, *, data: Data) -> int:
+    custom_dict = data.get_tsuneyo()
 
     return custom_dict[word]['accent'] if word in custom_dict else 0
 
-def get_definitions(word: str) -> str:
-    parser = etree.XMLParser(dtd_validation=True)
-    tree: etree._ElementTree = etree.parse("./data/JMdict_e_examp.xml", parser)
+def get_definitions(word: str, *, data: Data) -> str:
+    jisho = data.get_jisho()
     path = f"//keb[text()='{word}']/ancestor::entry/sense/gloss | //reb[text()='{word}']/ancestor::entry/sense/gloss"
-    print(path)
-    element: list[etree._Element] = tree.xpath(path)
+    element: list[etree._Element] = jisho.xpath(path)
 
     return [gloss.text for gloss in element]
 
 def main():
+    print("Setting up...")
+    sources = Data(
+        jisho_path="./data/JMdict_e_examp.xml",
+        tsuneyo_path="./data/ja_pitch_accents.tsv"
+    )
+
     selected_word = input("Which word would you like to use? > ")
-    reading = get_reading(selected_word)
-    pitch = get_accent(selected_word)
-    definitions = get_definitions(selected_word)
+    reading = get_reading(selected_word, data=sources)
+    pitch = get_accent(selected_word, data=sources)
+    definitions = get_definitions(selected_word, data=sources)
 
     wide_space = "　"
     print(_bold("今日の言葉"))
