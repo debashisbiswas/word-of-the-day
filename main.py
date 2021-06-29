d684439326308e263071943c8e547b03d34f3f8c
Debashis Biswas <debashis.biswas@asu.edu>
2021-06-29 14:47:26 -0700

    add example sentences, remove getters from data class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from lxml import etree
+import random
 
 class Data:
     def __init__(self, *, jisho_path, tsuneyo_path):
@@ -21,11 +22,10 @@ class Data:
                 }
         return custom_dict
 
-    def get_jisho(self):
-        return self.jisho
-
-    def get_tsuneyo(self):
-        return self.tsuneyo
+class SentencePair():
+    def __init__(self, jpn_sentence, eng_sentence):
+        self.jpn = jpn_sentence
+        self.eng = eng_sentence
 
 def _bold(str):
     return f"**{str}**"
@@ -34,21 +34,32 @@ def _spoiler(str):
     return f"||{str}||"
 
 def get_reading(word: str, *, data: Data) -> str:
-    custom_dict = data.get_tsuneyo()
+    custom_dict = data.tsuneyo
 
     return custom_dict[word]['kana'] if word in custom_dict else ''
 
 def get_accent(word: str, *, data: Data) -> int:
-    custom_dict = data.get_tsuneyo()
+    custom_dict = data.tsuneyo
 
     return custom_dict[word]['accent'] if word in custom_dict else 0
 
 def get_definitions(word: str, *, data: Data) -> str:
-    jisho = data.get_jisho()
+    jisho = data.jisho
     path = f"//keb[text()='{word}']/ancestor::entry/sense/gloss | //reb[text()='{word}']/ancestor::entry/sense/gloss"
     element: list[etree._Element] = jisho.xpath(path)
 
     return [gloss.text for gloss in element]
+
+def get_sentences(word: str, *, data: Data) -> list[SentencePair]:
+    jisho = data.jisho
+    path = f"//keb[text()='{word}']/ancestor::entry/sense/example | //reb[text()='{word}']/ancestor::entry/sense/example"
+    examples: list[etree._Element] = jisho.xpath(path)
+    sentences: list[SentencePair] = []
+    for example in examples:
+        jpn = example.xpath("ex_sent[@xml:lang='jpn']")[0].text
+        eng = example.xpath("ex_sent[@xml:lang='eng']")[0].text
+        sentences.append(SentencePair(jpn, eng))
+    return sentences
 
 def main():
     print("Setting up...")
@@ -61,7 +72,9 @@ def main():
     reading = get_reading(selected_word, data=sources)
     pitch = get_accent(selected_word, data=sources)
     definitions = get_definitions(selected_word, data=sources)
+    sentences = get_sentences(selected_word, data=sources)
 
+    sentence = random.choice(sentences)
     wide_space = "　"
     print(_bold("今日の言葉"))
     print()
@@ -72,6 +85,9 @@ def main():
         wide_space +
         _spoiler(f'{", ".join(definitions)}')
     )
+    print()
+    print(sentence.jpn)
+    print(_spoiler(sentence.eng))
 
 if __name__ == "__main__":
     main()
